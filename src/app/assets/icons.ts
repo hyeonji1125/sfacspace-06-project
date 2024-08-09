18e1281db87f334f8969a4e4fb8f9c464750c222
MinDDo <minzino4825@gmail.com>
2024-08-09 20:30:02 +0900

    design: Header, Footer UI
    - DarkMode 구현 추가 (수정 중)

---
+++ src/app/assets/icons.ts
@@ -0,0 +1,17 @@
+/* Header */
+import MainLogo from "./images/flaw-detector-main-logo.png"
+
+/* Footer */
+import FooterPattern from "./images/footer-pattern.png"
+import SfacLogo from "./images/footer-dark-sfac-logo.png"
+
+const icons = {
+  /* Header */
+  MainLogo,
+
+  /* Footer */
+  FooterPattern,
+  SfacLogo,
+};
+
+export default icons;
