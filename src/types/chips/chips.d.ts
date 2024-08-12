b75d6250ed8c10f4c0e1f878bb97a53bd420302a
minhoshin11 <tlsalsgh777@naver.com>
2024-08-12 20:43:45 +0900

    style: AssistChips,InputChips 컴포넌트 생성

---
+++ src/types/chips/chips.d.ts
@@ -0,0 +1,11 @@
+
+//InputChips 타입
+export type InputChipsSize = 'textOnly' | 'sideIcon' | 'percentage' | 'leftIcon'
+export type InputChipsColor = 'white'| 'purpleOne' | 'purpleTwo' | 'purpleThree' | 'purpleFour' | 'purpleFive' | 'purpleSix' | 'purpleSeven' | 'purpleEight' | 'purpleNine' 
+export type InputChipsShadow = "have"
+
+//AssistChips 타입
+export type AssistChipsText =  'purple' | 'white' | 'red' | 'redLight'
+export type AssistChipsBg = 'white' | 'grayLight' | 'grayNormal' | 'grayDark' | 'purple' | 'blue' | 'red' | 'redLight'
+export type AssistChipsBorder = 'black' | 'purpleLight' | 'purpleMiddle' | 'purpleDark'
+export type AssistChipsShadow  = 'have'
