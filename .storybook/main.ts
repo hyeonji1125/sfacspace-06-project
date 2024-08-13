ee663951c478bbd79893b2596950a418d1ee193d
aoooec <foottable@gmail.com>
2024-08-13 17:40:56 +0900

    fix: staticDirs 경로 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,6 +13,6 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
-  staticDirs: ["..\\public"],
+  staticDirs: [path.resolve(__dirname, "../public")],
 };
 export default config;
