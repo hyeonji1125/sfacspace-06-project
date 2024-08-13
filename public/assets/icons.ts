4bc08e009248643d7e1e4b8a1bf319b3d0aa2d48
MinDDo <minzino4825@gmail.com>
2024-08-13 09:34:14 +0900

    fix: icons.ts png->svg 변경

--- public/assets/icons.ts
+++ public/assets/icons.ts
@@ -1,17 +1,9 @@
 /* Header */
-<<<<<<< HEAD
-import MainLogo from "./images/flaw-detector-main-logo.svg";
+import MainLogo from "./images/flaw-detector-main-logo.png"
 
 /* Footer */
 import FooterPattern from "./images/footer-pattern.svg";
 import SfacLogo from "./images/footer-sfacspace-logo.svg";
-=======
-import MainLogo from "./images/flaw-detector-main-logo.png"
-
-/* Footer */
-import FooterPattern from "./images/footer-pattern.png"
-import SfacLogo from "./images/footer-dark-sfac-logo.png"
->>>>>>> 715c231 (feat: Dropdown 컴포넌트 구현 및 다크모드 수정)
 
 const icons = {
   /* Header */
@@ -22,8 +14,5 @@ const icons = {
   SfacLogo,
 };
 
-<<<<<<< HEAD
 export default icons;
-=======
-export default icons;
->>>>>>> 715c231 (feat: Dropdown 컴포넌트 구현 및 다크모드 수정)
+
