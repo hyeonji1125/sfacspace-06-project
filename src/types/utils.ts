ed31cbd267086a61f5c42263949802d4a99f6ab8
MinDDo <minzino4825@gmail.com>
2024-08-13 10:20:28 +0900

    refact: Change interface to type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,15 +1,15 @@
-export interface SvgState {
+export type SvgState = {
   className?: string;
-}
+};
 
-export interface DarkModeState {
+export type DarkModeState = {
   darkMode: boolean;
   setDarkMode: (value: boolean) => void;
-
   toggleDarkMode: () => void;
-}
-export interface DropdownProps {
+};
+
+export type DropdownProps = {
   type: 'Type' | 'Sort';
   selectedOption: string;
   onSelect: (option: string) => void;
-}
+};
