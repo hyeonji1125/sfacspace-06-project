23cd1ef395fab0b405f0aa3b8d997cdc2ed82ff7
ko9612 <kohana735@gmail.com>
2024-08-13 20:50:39 +0900

    fix: logo 정렬 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,10 +5,10 @@ import { Aldrich } from "next/font/google";
 import {
   LightModeIcon,
   DarkModeIcon,
+  FlawDetectorLogo,
 } from "../../../public/assets/svg/SvgIcons";
 import icons from "../../../public/assets/icons";
 import Image from "next/image";
-import { twJoin } from "tailwind-merge";
 import { useTheme } from "next-themes"; // useTheme 훅을 사용하여 테마 관리
 
 const aldrich = Aldrich({ weight: "400", subsets: ["latin"] });
@@ -23,30 +23,30 @@ const Header: React.FC = () => {
   return (
     <header className="sticky w-full bg-custom-light-bg px-4 py-4 dark:bg-custom-dark-bg md:px-20 md:py-12">
       <div className="flex items-center justify-between text-custom-light-text dark:text-custom-dark-text">
-        <h1 className="flex items-center">
+        <h1>
           <Image
             src={icons.MainLogo}
-            alt="Flaw Detector Logo"
-            className="h-[30px] w-[30px] md:h-[35px] md:w-[35px]"
+            alt="FlawDetector"
+            className="h-[30px] w-[30px] md:hidden"
           />
-          <span
-            className={twJoin(
-              "ml-2 hidden text-center text-[20px] tracking-[-0.01em] text-black dark:text-custom-dark-text md:ml-4 md:block md:text-[40px] md:mt-2",
-              aldrich.className,
-            )}
-          >
-            FLAWDETECTOR
-          </span>
+          <FlawDetectorLogo
+            className={"hidden md:block"}
+            fill={theme === "dark" ? "white" : "black"}
+          />
         </h1>
         <div className="flex items-center space-x-4 text-[14px] font-medium md:space-x-8 md:text-[18px]">
           <span>취약점 DB</span>
           <span>MY 저장소</span>
           <div
-            className="flex items-center justify-center rounded-full border-2 p-1 cursor-pointer"
+            className="flex cursor-pointer items-center justify-center rounded-full border-2 p-1"
             onClick={toggleTheme}
           >
-            <LightModeIcon className={`${theme === "dark" ? "hidden" : "block"}`} />
-            <DarkModeIcon className={`${theme === "dark" ? "block" : "hidden"}`} />
+            <LightModeIcon
+              className={`${theme === "dark" ? "hidden" : "block"}`}
+            />
+            <DarkModeIcon
+              className={`${theme === "dark" ? "block" : "hidden"}`}
+            />
           </div>
         </div>
       </div>
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,5 +1,6 @@
 export type SvgState = {
   className?: string;
+  fill?:string;
 };
 
 export type DarkModeState = {
