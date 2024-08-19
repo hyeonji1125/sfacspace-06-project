d84b52ea926102bf09dbe08ec5a85b47f2f0044e
aoooec <foottable@gmail.com>
2024-08-19 11:47:57 +0900

    fix: SecureProcess 다크모드 bg 수정

--- src/app/(home)/_components/SecureProcessSection.tsx
+++ src/app/(home)/_components/SecureProcessSection.tsx
@@ -2,17 +2,17 @@ import ServiceCardsGrid from "./Card/ServiceCardsGrid";
 
 export default function SecureProcessSection() {
   return (
-    <div className="flex h-[1024px] w-full flex-col justify-center bg-primary-purple-500 pt-[100px] dark:bg-custom-dark-bg">
-        <div className="mb-[80px] flex justify-center text-center text-4xl font-bold leading-[50px] text-white dark:text-custom-dark-text md:text-6xl md:leading-[73px]">
-          안전과 보호를 우선으로 하는
-          <br />
-          프로세스를 제공합니다.
-        </div>
+    <div className="flex h-[1024px] w-full flex-col justify-center bg-primary-purple-500 pt-[100px]">
+      <div className="mb-[80px] flex justify-center text-center text-4xl font-bold leading-[50px] text-white dark:text-custom-dark-text md:text-6xl md:leading-[73px]">
+        안전과 보호를 우선으로 하는
+        <br />
+        프로세스를 제공합니다.
+      </div>
       <div className="relative flex h-[420px] w-full overflow-hidden md:h-[550px]">
-        <div className="animate-rotateForward absolute z-[1] flex">
+        <div className="absolute z-[1] flex animate-rotateForward">
           <ServiceCardsGrid />
         </div>
-        <div className="animate-rotateBackward absolute z-[1] flex">
+        <div className="absolute z-[1] flex animate-rotateBackward">
           <ServiceCardsGrid />
         </div>
       </div>
