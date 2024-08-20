e31a1ebd3966deb52b350ce77fd0834e8930e02b
apple <apple@appleui-MacBookPro.local>
2024-08-20 15:43:22 +0900

    chore:css오타수정

--- src/app/(home)/_components/SecureProcessSection.tsx
+++ src/app/(home)/_components/SecureProcessSection.tsx
@@ -2,7 +2,7 @@ import ServiceCardsGrid from "./Card/ServiceCardsGrid";
 
 export default function SecureProcessSection() {
   return (
-    <section className="lex h-full w-full flex-col justify-center bg-primary-purple-500 pt-[100px] md:h-[900px]">
+    <section className="flex h-full w-full flex-col justify-center bg-primary-purple-500 pt-[100px] md:h-[900px]">
       <div className="mb-[80px] flex justify-center px-[10%] text-center text-2xl font-bold leading-[35px] text-white dark:text-custom-dark-text sm:text-4xl md:text-5xl md:leading-[49px] xl:text-6xl xl:leading-[73px]">
         안전과 보호를 우선으로 하는
         <br />
