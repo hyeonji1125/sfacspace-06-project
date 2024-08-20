029be8b7d39de329b9d0341bcf77abf2e514023d
apple <apple@appleui-MacBookPro.local>
2024-08-20 15:23:21 +0900

    design:SecureProcessSection css수정

--- src/app/(home)/_components/SecureProcessSection.tsx
+++ src/app/(home)/_components/SecureProcessSection.tsx
@@ -2,8 +2,8 @@ import ServiceCardsGrid from "./Card/ServiceCardsGrid";
 
 export default function SecureProcessSection() {
   return (
-    <div className="flex h-[1024px] w-full flex-col justify-center bg-primary-purple-500 pt-[100px]">
-      <div className="mb-[80px] flex justify-center text-center text-4xl font-bold leading-[50px] text-white dark:text-custom-dark-text md:text-6xl md:leading-[73px]">
+    <section className="lex h-full w-full flex-col justify-center bg-primary-purple-500 pt-[100px] md:h-[900px]">
+      <div className="mb-[80px] flex justify-center px-[10%] text-center text-2xl font-bold leading-[35px] text-white dark:text-custom-dark-text sm:text-4xl md:text-5xl md:leading-[49px] xl:text-6xl xl:leading-[73px]">
         안전과 보호를 우선으로 하는
         <br />
         프로세스를 제공합니다.
@@ -16,6 +16,6 @@ export default function SecureProcessSection() {
           <ServiceCardsGrid />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
