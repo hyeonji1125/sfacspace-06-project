abac85803f6f0dc0df25bd1ba4cf5607b65b943f
aoooec <foottable@gmail.com>
2024-08-23 15:38:06 +0900

    feat: UserItem 컴포넌트 추가(#47)

---
+++ src/app/(my-repo)/_components/UserItem.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Button from "@/components/common/Button";
+import UserPic from "@/components/common/UserPic";
+import { signOut, useSession } from "next-auth/react";
+
+export default function UserItem() {
+  const { data, status } = useSession();
+  const { user } = data ?? {};
+
+  if (status === "loading") {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <>
+      {user && user.image && user.name && (
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-11">
+            <UserPic image={user.image} name={user.name} />
+            <p className="flex flex-col text-[40px] font-medium leading-[1.2] tracking-tighter text-text-gray-dark dark:text-custom-dark-text">
+              <span>Hello,</span>
+              <span>{user?.email}</span>
+            </p>
+          </div>
+          <Button
+            theme="outlined"
+            size="small"
+            className="border-2 border-primary-purple-500 font-normal"
+            onClick={() => signOut()}
+          >
+            로그아웃
+          </Button>
+        </div>
+      )}
+    </>
+  );
+}
