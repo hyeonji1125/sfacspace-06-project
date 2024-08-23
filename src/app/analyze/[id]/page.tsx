bc22ba6584265d298df482701a2b55df72860ce7
hana ko <kohana735@gmail.com>
2024-08-24 06:25:03 +0900

    feat: 검사 버튼 클릭시 나오는 모달 작성

--- src/app/analyze/[id]/page.tsx
+++ src/app/analyze/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { IoIosArrowBack, IoIosClose } from "react-icons/io";
+"use client";
+import { IoIosArrowBack } from "react-icons/io";
 import FileTabs from "./_components/FileTabs";
 import Button from "@/components/common/Button";
 import Infobox from "./_components/Infobox";
@@ -7,8 +8,32 @@ import FileViewer from "./_components/FileViewer";
 import CompletedFileViewer from "./_components/CompletedFileViewer";
 import ModifiedCode from "./_components/ModifiedCode";
 import PcOnlyMessage from "@/components/common/PcOnlyMessage";
+import AnalyzeModal from "./_components/AnalyzeModal";
+import { useState } from "react";
+import { useFileStore } from "@/store/useFileStore";
 
 export default function AnalyzePage() {
+  // 임시 code
+  const list = useFileStore((state) => state.list);
+  const selectedFiles = list.filter((item) => item.isSelected);
+
+  const [isOpen, setIsOpen] = useState(false); // 모달 state
+  const [isWhole, setIsWhole] = useState(false); // 파일 전체를 포함하는 지
+
+  const handleWholeButton = () => {
+    setIsOpen(true);
+    setIsWhole(true);
+  };
+
+  const handleButton = () => {
+    setIsOpen(true);
+    if (list.length === selectedFiles.length) {
+      setIsWhole(true);
+    } else {
+      setIsWhole(false);
+    }
+  };
+
   return (
     <>
       <PcOnlyMessage />
@@ -23,7 +48,12 @@ export default function AnalyzePage() {
 
         {/* 메인 컨텐츠 영역 */}
         <div className="flex items-stretch gap-7">
-          <Button type="button" theme={"filled"} className="w-[247px]">
+          <Button
+            type="button"
+            theme={"filled"}
+            className="w-[247px]"
+            onClick={handleWholeButton}
+          >
             폴더 전체 검사
           </Button>
           <FileTabs />
@@ -34,11 +64,17 @@ export default function AnalyzePage() {
           <div className="flex flex-col justify-stretch gap-6">
             <Infobox />
             <FileList />
-            <Button type="button" theme={"filled"} className="w-[247px]">
+            <Button
+              type="button"
+              theme={"filled"}
+              className="w-[247px]"
+              onClick={handleButton}
+              disabled={!selectedFiles.length}
+            >
               검사하기
             </Button>
           </div>
-          {/* 중앙 컨텐츠 */}
+          {/*File Viewer*/}
           <div className="flex flex-1 flex-col gap-14">
             <div className="grid grid-cols-2 gap-4">
               <FileViewer />
@@ -47,6 +83,12 @@ export default function AnalyzePage() {
             <ModifiedCode />
           </div>
         </div>
+        <AnalyzeModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          isWhole={isWhole}
+          selectedFiles={selectedFiles}
+        />
       </section>
     </>
   );
