89acc19088fc76385413ed0659d781f531637e9a
aoooec <foottable@gmail.com>
2024-08-26 02:35:37 +0900

    fix: my-repo 레이아웃 수정 및 me 하위 레이아웃 추가(#58)

--- src/app/(my-repo)/layout.tsx
+++ src/app/(my-repo)/layout.tsx
@@ -4,8 +4,6 @@ export default function MyRepoLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto mb-[124px] mt-[72px] flex w-full min-w-[1024px] max-w-[1314px] flex-col items-center gap-[124px] px-20">
-      {children}
-    </div>
+    <div className="flex w-full items-center justify-center">{children}</div>
   );
 }
---
+++ src/app/(my-repo)/me/layout.tsx
@@ -0,0 +1,11 @@
+export default function MyPageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="mx-auto mb-[124px] mt-[72px] flex w-full min-w-[1024px] max-w-[1314px] flex-col items-center gap-[124px] px-20">
+      {children}
+    </div>
+  );
+}
