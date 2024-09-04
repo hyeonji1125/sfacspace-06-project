5baa495001a1baa22a96dad9ea1c846da3ad0388
aoooec <foottable@gmail.com>
2024-09-05 00:09:02 +0900

    feat: fetchRepos 유틸함수 생성 (#70)

---
+++ src/app/repos/_utils/fetchRepos.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/firebase";
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
+
+export const getReposData = async (email: string) => {
+  try {
+    const reposCollectionRef = collection(db, `users/${email}/repos`);
+    const querySnapshot = await getDocs(reposCollectionRef);
+    console.log(querySnapshot);
+    return querySnapshot;
+  } catch (error) {
+    throw new Error("DB에서 Repos Data를 받아오는데 실패했습니다.");
+  }
+};
+
+export const postRepo = async (email: string, repoName: string, data: any) => {
+  try {
+    const repoDocRef = doc(collection(db, `users/${email}/repos`), repoName);
+    await setDoc(repoDocRef, data, { merge: true });
+  } catch (error) {
+    throw new Error("DB에서 Repo Data를 업데이트 하는데 실패했습니다.");
+  }
+};
