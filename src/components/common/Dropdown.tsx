75177f03b1e9e8ea01d8e93f57859128c5ed773c
aoooec <foottable@gmail.com>
2024-09-13 01:13:55 +0900

    design: 드롭다운 css 수정

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -29,15 +29,12 @@ const Dropdown: React.FC<TDropdownProps> = ({
   };
 
   return (
-    <div
-      className="relative z-10 inline-block text-left"
-      style={{ width: "auto" }}
-    >
+    <div className="relative z-10 inline-block w-auto min-w-[100px] text-left">
       <div>
         <button
           type="button"
           className={twMerge(
-            "flex w-full items-center justify-between gap-1 rounded-lg border px-[13px] py-[10px] text-sm shadow-sm focus:outline-none",
+            "flex h-11 w-full items-center justify-between gap-1 rounded-lg border px-[13px] py-[10px] text-sm shadow-sm focus:outline-none",
             "border-text-gray-default bg-white dark:bg-custom-dropdown-dark-bg",
             "text-custom-light-text dark:text-custom-dark-text",
             "hover:bg-gray-50",
@@ -57,24 +54,27 @@ const Dropdown: React.FC<TDropdownProps> = ({
             "absolute right-0 mt-2 w-full origin-top-right rounded-md",
           )}
         >
-          <div className="py-1">
+          <div className="rounded-lg shadow-md">
             {options.map((option, index) => (
               <button
                 key={option}
                 onClick={() => handleOptionClick(option)}
                 className={twMerge(
-                  "flex w-full items-center justify-center gap-2 py-[10px] text-center text-[14px]",
+                  "flex w-full items-center justify-center gap-2 py-[10px] text-center text-base hover:bg-bg-purple-dark dark:hover:bg-grayscale-80",
                   "text-custom-light-text dark:text-custom-dark-text",
-                  "bg-white shadow-2xl dark:bg-custom-dropdown-dark-bg",
+                  "bg-white dark:bg-custom-dropdown-dark-bg",
                   option === selectedOption
-                    ? "bg-custom-dropdown-option-light-bg font-bold dark:bg-gray-200 dark:text-custom-light-text"
+                    ? "bg-custom-dropdown-option-light-bg dark:bg-grayscale-80"
                     : "",
                   index === 0 ? "rounded-t-md" : "",
                   index === options.length - 1 ? "rounded-b-md" : "",
                 )}
               >
                 {option === selectedOption && (
-                  <DropdownCheckIcon className="h-5 w-5" />
+                  <DropdownCheckIcon
+                    className="h-5 w-5"
+                    color="dark:fill-custom-dark-text"
+                  />
                 )}
                 <span>{option}</span>
               </button>
