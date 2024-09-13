80acb97504260b3110004b5d9fbe982bd472e2f5
minhoshin11 <tlsalsgh777@naver.com>
2024-09-13 14:15:54 +0900

    useParams훅으로 인한 오류 수정

--- src/app/vuldb/page.tsx
+++ src/app/vuldb/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import MainPostSection from "./_components/MainPostSection";
 import SearchBar from "./_components/SearchBar";
 import TopicList from "./_components/TopicList";
@@ -7,7 +8,9 @@ export default function VulnerabilityDb() {
     <main className="h-auto max-w-[1920px] px-6 pt-9">
       <div className="mx-auto flex w-full max-w-[1313px] flex-col gap-[76px]">
         {/* <ImageCardList /> */}
-        <SearchBar />
+        <Suspense fallback={<div>로딩중..</div>}>
+          <SearchBar />
+        </Suspense>
         <div className="flex justify-between gap-4">
           <MainPostSection />
           <TopicList />
