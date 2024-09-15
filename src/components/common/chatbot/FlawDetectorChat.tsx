19a43267bdf5df47f8659ba2746caba059960cd7
aoooec <foottable@gmail.com>
2024-09-15 18:17:48 +0900

    add: Chatbot 컴포넌트 생성 및 연결 (#87)

---
+++ src/components/common/chatbot/FlawDetectorChat.tsx
@@ -0,0 +1,36 @@
+import { Bug } from "../../../../public/assets/svg/SvgIcons";
+import ChatBubble from "./ChatBubble";
+
+export default function FlawDetectorChat({
+  children,
+  created_at,
+  isLoading = false,
+}: {
+  children?: React.ReactNode;
+  created_at?: string;
+  isLoading?: boolean;
+}) {
+  return (
+    <div className="flex w-full gap-3">
+      <div className="flex h-[52px] w-[52px] items-center justify-center rounded-[18px] bg-primary-purple-500 p-3">
+        <Bug className="h-full w-full" />
+      </div>
+      <div className="flex flex-col gap-[6px]">
+        <span className="select-none text-lg font-medium leading-6 text-[#212122]">
+          플로디텍터 운영자
+        </span>
+        <ChatBubble sender="AI" created_at={created_at}>
+          {isLoading ? (
+            <div className="flex gap-1 px-1 py-2">
+              <div className="h-1 w-1 animate-bounce rounded-full bg-text-gray-default" />
+              <div className="h-1 w-1 animate-bounce rounded-full bg-text-gray-default [animation-delay:-.3s]" />
+              <div className="h-1 w-1 animate-bounce rounded-full bg-text-gray-default [animation-delay:-.5s]" />
+            </div>
+          ) : (
+            children
+          )}
+        </ChatBubble>
+      </div>
+    </div>
+  );
+}
