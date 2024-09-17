d8dcd6cb535a676370b2af7cb37edeb949a85ff8
aoooec <foottable@gmail.com>
2024-09-17 23:48:28 +0900

    fix: me 페이지 하위 1280 이하 PcOnly 컴포넌트 노출로 변경

--- src/app/me/(me-layout)/layout.tsx
+++ src/app/me/(me-layout)/layout.tsx
@@ -1,11 +1,16 @@
+import PcOnlyMessage from "@/components/common/PcOnlyMessage";
+
 export default function MyPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto mb-[124px] mt-[72px] flex w-full min-w-[1024px] max-w-[1314px] flex-col items-center gap-[124px] px-20">
-      {children}
-    </div>
+    <>
+      <PcOnlyMessage />
+      <div className="mx-auto mb-[124px] mt-[72px] hidden w-full min-w-[1024px] max-w-[1314px] flex-col items-center gap-[124px] px-20 xl:flex">
+        {children}
+      </div>
+    </>
   );
 }
