63f21e7158acc905baf649cb86edf0cc5eb767bf
aoooec <foottable@gmail.com>
2024-09-19 22:58:23 +0900

    fix: ChatContainer 작은 화면에서의 반응형 추가

--- src/components/common/chatbot/ChatContainer.tsx
+++ src/components/common/chatbot/ChatContainer.tsx
@@ -4,7 +4,7 @@ import ChatLog from "./ChatLog";
 
 export default function ChatContainer() {
   return (
-    <section className="ml-5 flex h-[620px] flex-col justify-between overflow-hidden rounded-3xl border border-white bg-white shadow-lg dark:border-primary-purple-300 dark:bg-grayscale-90 sm:ml-0 sm:w-[480px]">
+    <section className="ml-5 flex h-[620px] max-h-[80vh] flex-col justify-between overflow-hidden rounded-3xl border border-white bg-white shadow-lg dark:border-primary-purple-300 dark:bg-grayscale-90 sm:ml-0 sm:w-[480px]">
       <div className="flex h-[67px] w-full select-none items-center gap-[10px] bg-primary-purple-500 p-6 dark:bg-primary-purple-300">
         <IoChatbubble className="h-[32px] w-[32px] fill-white dark:fill-custom-dark-bg" />
         <p className="text-xl font-semibold text-white dark:text-custom-dark-bg">
