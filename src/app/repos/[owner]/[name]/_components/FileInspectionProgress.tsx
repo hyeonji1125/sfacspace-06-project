61fd65231e81505721b703db231903ea220d00b2
hana ko <kohana735@gmail.com>
2024-10-13 18:49:55 +0900

    fix: 파일 분석 결과 box 디자인 수정(이후 기능 추가 필요)

--- src/app/repos/[owner]/[name]/_components/FileInspectionProgress.tsx
+++ src/app/repos/[owner]/[name]/_components/FileInspectionProgress.tsx
@@ -1,33 +1,44 @@
+import { useSession } from "next-auth/react";
+import { IoIosCode } from "react-icons/io";
 import { IoClose, IoEllipse, IoTriangle } from "react-icons/io5";
 
 export default function FileInspectionProgress() {
   const inspectionData = [
     {
-      icon: <IoClose viewBox="150 150 212 212" className="text-accent-red" />,
+      icon: <IoIosCode className="text-3xl text-accent-blue" />,
+      label: "검사한 파일 개수",
+      count: 0,
+    },
+    {
+      icon: <IoClose className="text-3xl text-accent-red" />,
       label: "검출된 취약점",
-      count: 12,
+      count: 0,
     },
     {
-      icon: (
-        <IoTriangle
-          viewBox="30 30 452 452"
-          className="text-xl text-accent-orange"
-        />
-      ),
-      label: "수정 제안",
-      count: 8,
-    },
-    {
-      icon: (
-        <IoEllipse
-          viewBox="50 50 412 412"
-          className="text-xl text-accent-green"
-        />
-      ),
+      icon: <IoEllipse className="ml-1 text-2xl text-accent-green" />,
       label: "문제 없음",
-      count: 23,
+      count: 0,
     },
   ];
+
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <ul className="flex animate-pulse flex-col gap-4 px-2">
+        {[...Array(3)].map((_, index) => (
+          <li key={index} className="flex justify-between text-xl">
+            <div className="flex items-center gap-[10px]">
+              <div className="h-7 w-7 rounded-full bg-grayscale-10 dark:bg-grayscale-80"></div>
+              <div className="h-7 w-28 rounded bg-grayscale-10 dark:bg-grayscale-80"></div>
+            </div>
+            <div className="h-7 w-8 rounded bg-grayscale-10 dark:bg-grayscale-80"></div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <ul className="flex flex-col gap-4 px-2">
       {inspectionData.map((item, index) => (
